010d21ea401392dcbfe6759fb3261b6067c9f910
lipenghuihenu <493284461@qq.com>
2022-03-12 18:57:02 +0800

    +1

--- CCF-Practice/src/t202104/t2.java
+++ CCF-Practice/src/t202104/t2.java
@@ -21,26 +21,17 @@ public class t2 {
 		}
 		for(int i=1;i<=n;i++) {
 			for(int j=1;j<=n;j++) {
-				int x1=i-r-1;
-				if(x1<1)x1=1;
-				int y1=j-r-1;
-				if(y1<1)y1=1;
-				
-				int x2=i+r;
-				int y2=j+r;
-				
-				if(x2>n)x2=n;
-				if(y2>n)y2=n;
-				int k=s[x2][y2]-s[x1][y2]-s[x2][y1]+s[x1][y1];
-				if(k<=r)sum++;
+				int x1=Math.min(i+r, n);
+				int y1=Math.min(j+r, n);
+				int x2=Math.max(i-r-1, 0);
+				int y2=Math.max(j-r-1, 0);
+				int x3=Math.max(i-r, 1);
+				int y3=Math.max(j-r, 1);
+				int k=s[x1][y1]-s[x2][y1]-s[x1][y2]+s[x2][y2];
+				int p=(x1-x3+1)*(y1-y3+1);
+				if(k/1.0/p<=t)sum++;
 			}
 		}
 		System.out.println(sum);
-//		for(int i=1;i<=n;i++) {
-//			for(int j=1;j<=n;j++) {
-//				System.out.print(s[i][j]+" ");
-//			}
-//			System.out.println();
-//		}
 	}
 }
---
+++ CCF-Practice/src/t202104/t3.java
@@ -0,0 +1,7 @@
+package t202104;
+
+public class t3 {
+	public static void main(String[] args) {
+			
+	}
+}
