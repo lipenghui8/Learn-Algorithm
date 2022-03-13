31d6a312508be252b4f0bff9a127945e36422e61
lipenghuihenu <493284461@qq.com>
2022-03-13 22:34:55 +0800

    +1

--- Java-LanQiao/src/t2020_1/八次求和.java
+++ Java-LanQiao/src/t2020_1/八次求和.java
@@ -1,5 +1,21 @@
 package t2020_1;
 
-public class 八次求和 {
+import java.math.BigInteger;
+import java.util.Scanner;
 
+public class 八次求和 {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		sc.close();
+		BigInteger sum=BigInteger.ZERO;
+		BigInteger s=new BigInteger("123456789");
+		for(int i=1;i<=n;i++) {
+			BigInteger p=new BigInteger(Integer.toString(i));
+			p=p.pow(8);
+			sum=sum.add(p);
+			sum=sum.mod(s);
+		}
+		System.out.println(sum.toString());
+	}
 }
