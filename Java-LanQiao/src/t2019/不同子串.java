4e100a6977c1c5c94a3ff45cb0e2bf057a278d81
lipenghuihenu <493284461@qq.com>
2022-03-23 17:43:02 +0800

    +1

--- Java-LanQiao/src/t2019/不同子串.java
+++ Java-LanQiao/src/t2019/不同子串.java
@@ -1,7 +1,18 @@
 package t2019;
 
+import java.util.Set;
+import java.util.TreeSet;
+
 public class 不同子串 {
+	static String a="0100110001010001";
+	static Set<String> set=new TreeSet<>();
 	public static void main(String[] args) {
-		
+		for(int i=0;i<a.length();i++) {
+			for(int j=i+1;j<=a.length();j++){
+				String ssString=a.substring(i,j);
+				set.add(ssString);
+			}
+		}
+		System.out.println(set.size());
 	}
 }
