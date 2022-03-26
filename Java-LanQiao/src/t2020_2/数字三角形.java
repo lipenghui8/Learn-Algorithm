1d9476f9131bca4d2206711c1437f293e0689613
lipenghuihenu <493284461@qq.com>
2022-03-26 19:42:28 +0800

    +1

--- Java-LanQiao/src/t2020_2/数字三角形.java
+++ Java-LanQiao/src/t2020_2/数字三角形.java
@@ -1,7 +1,23 @@
 package t2020_2;
 
+import java.util.Scanner;
+
 public class 数字三角形 {
+	static int[][] s=new int[200][200];
+	static int n;
 	public static void main(String[] args) {
-		
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		for(int i=1;i<=n;i++) {
+			for(int j=1;j<=i;j++) {
+				s[i][j]=sc.nextInt();
+				s[i][j]+=Math.max(s[i-1][j-1],s[i-1][j]);
+			}
+		}
+		if(n%2==1) {
+			System.out.println(s[n][n/2+1]);
+		}else {
+			System.out.println(Math.max(s[n][n/2], s[n][n/2+1]));
+		}		
 	}
 }
