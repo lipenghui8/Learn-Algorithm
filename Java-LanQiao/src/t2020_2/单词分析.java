29da604e4df61cc3f36947df0227984ea6a64bdf
lipenghuihenu <493284461@qq.com>
2022-03-26 18:54:11 +0800

    +1

---
+++ Java-LanQiao/src/t2020_2/单词分析.java
@@ -0,0 +1,7 @@
+package t2020_2;
+
+public class 单词分析 {
+	public static void main(String[] args) {
+		
+	}
+}
--- Java-LanQiao/src/t2020_2/成绩分析.java
+++ Java-LanQiao/src/t2020_2/成绩分析.java
@@ -1,7 +1,24 @@
 package t2020_2;
 
+import java.util.Scanner;
+
 public class 成绩分析 {
 	public static void main(String[] args) {
-		
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		long max=Integer.MIN_VALUE,min=Integer.MAX_VALUE,sum=0;
+		for(int i=1;i<=n;i++) {
+			int x=sc.nextInt();
+			if(x>max) {
+				max=x;
+			}
+			if(x<min) {
+				min=x;
+			}
+			sum+=x;
+		}
+		System.out.println(max);
+		System.out.println(min);
+		System.out.println(String.format("%.2f", sum/1.0/n));
 	}
 }
