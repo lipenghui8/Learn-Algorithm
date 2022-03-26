a306be5858ec6c4a8ab21592d2e1a98af36eb2e3
lipenghuihenu <493284461@qq.com>
2022-03-26 16:26:59 +0800

    +1

--- Java-LanQiao/src/t2019/后缀表达式_正解.java
+++ Java-LanQiao/src/t2019/后缀表达式_正解.java
@@ -5,6 +5,37 @@ import java.util.Scanner;
 public class 后缀表达式_正解 {
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
-		
+		int n=sc.nextInt(),m=sc.nextInt();
+		int[] s=new int[m+n+1+10];
+		long minz=Integer.MAX_VALUE,maxf=Integer.MIN_VALUE,sumn=0,sumabs=0,numz=0,numf=0;
+		for(int i=1;i<=m+n+1;i++) {
+			s[i]=sc.nextInt();
+			sumn+=s[i];
+			sumabs+=Math.abs(s[i]);
+			if(s[i]>0) {
+				minz=Math.min(minz, s[i]);
+				numz++;
+			}else{
+				maxf=Math.max(maxf, s[i]);
+				numf++;
+			}
+		}
+		if(numf==m) {
+			System.out.println(sumabs);
+		}else if(numf>m) {
+			if(numf==(n+m+1)) {
+				System.out.println(sumabs+2*maxf);
+			}else if(m==0) {
+				System.out.println(sumn);
+			}else {
+				System.out.println(sumabs);
+			}
+		}else if(numf<m) {
+			if(numf==0) {
+				System.out.println(sumabs-2*minz);
+			}else {
+				System.out.println(sumabs);
+			}
+		}
 	}
 }
