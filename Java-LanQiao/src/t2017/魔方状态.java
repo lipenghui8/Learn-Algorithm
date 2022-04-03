212463573f89bb3d51121219c7e5ab2c812202ca
lipenghuihenu <493284461@qq.com>
2022-04-03 16:35:02 +0800

    +1

---
+++ Java-LanQiao/src/t2017/魔方状态.java
@@ -0,0 +1,20 @@
+package t2017;
+
+public class 魔方状态 {
+	static int[] s= {1,1,2,2,3,3};
+	static int[] v= new int[25];
+	static int[] a= new int[25];
+	static long sum=0;
+	static void dfs(int x) {
+		if(x>24) {
+			
+			return;
+		}
+		for(int i=1;i<=6;i++) {
+			
+		}
+	}
+	public static void main(String[] args) {
+		dfs(1);
+	}
+}
