bd3cc8aad627a641e678224aa4eb3f05fb628757
lipenghuihenu <493284461@qq.com>
2022-04-03 14:58:11 +0800

    +1

--- Java-LanQiao/src/t2018/方格计数.java
+++ Java-LanQiao/src/t2018/方格计数.java
@@ -3,6 +3,14 @@ package t2018;
 
 public class 方格计数 {
 	public static void main(String[] args) {
-		System.out.println(4000000);
+		int sum=0;
+		for(int i=-1000;i<=-1;i++) {
+			for(int j=1000;j>=1;j--) {
+				if(Math.sqrt(i*1.0*i+j*j)<=1000) {
+					sum++;
+				}
+			}
+		}
+		System.out.println(sum*4);
 	}
 }
