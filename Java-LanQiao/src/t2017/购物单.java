77f0a3df896e8b22c5d6202b7d98a04db83174ad
lipenghuihenu <493284461@qq.com>
2022-04-03 15:34:02 +0800

    +1

--- Java-LanQiao/src/t2017/购物单.java
+++ Java-LanQiao/src/t2017/购物单.java
@@ -1,7 +1,28 @@
 package t2017;
 
 public class 购物单 {
+	static int[] a=new int[20];
+	static int[] v=new int[20];
+	static int sum=0;
+	static void dfs(int x) {
+		if(x>9) {
+			if((a[1]+a[2]+a[3]+a[4])==(a[4]+a[5]+a[6]+a[7])&&(a[4]+a[5]+a[6]+a[7])==(a[1]+a[7]+a[8]+a[9])) {
+				sum+=1;
+				
+			}
+			return;
+		}
+		for(int i=1;i<=9;i++) {
+			if(v[i]==0) {
+				v[i]=1;
+				a[x]=i;
+				dfs(x+1);
+				v[i]=0;
+			}
+		}
+	}
 	public static void main(String[] args) {
-		
+		dfs(1);
+		System.out.println(sum/6);
 	}
 }
