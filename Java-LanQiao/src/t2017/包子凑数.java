9b6a277383b4fffbee67cbb4150178ccd3129223
lipenghuihenu <493284461@qq.com>
2022-04-04 10:53:18 +0800

    +1

--- Java-LanQiao/src/t2017/包子凑数.java
+++ Java-LanQiao/src/t2017/包子凑数.java
@@ -1,7 +1,13 @@
 package t2017;
 
+import java.util.Date;
+
 public class 包子凑数 {
+	@SuppressWarnings("deprecation")
 	public static void main(String[] args) {
-		
+		Date d1=new Date(21,7,23,12,0,0);
+		Date d2=new Date(120,7,1,12,0,0);
+		long time=d2.getTime()-d1.getTime();
+		System.out.println(time/60000);
 	}
 }
