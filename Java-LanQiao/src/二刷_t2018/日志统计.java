e20cde253d76aca3c24e93289a23c261f11f7413
lipenghuihenu <493284461@qq.com>
2022-04-08 16:29:59 +0800

    +1

---
+++ Java-LanQiao/src/二刷_t2018/日志统计.java
@@ -0,0 +1,7 @@
+package 二刷_t2018;
+
+public class 日志统计 {
+	public static void main(String[] args) {
+		
+	}
+}
--- Java-LanQiao/src/二刷_t2018/螺旋折线.java
+++ Java-LanQiao/src/二刷_t2018/螺旋折线.java
@@ -1,7 +1,56 @@
 package 二刷_t2018;
 
+import java.util.Scanner;
+
 public class 螺旋折线 {
-	public static void main(String[] args) {
-		
-	}
+    static long x, y;
+    static long df = 0L;
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        x = in.nextInt();
+        y = in.nextInt();
+
+        if (x > 0 && y >= 0) {
+        	long dx = Math.max(x, y);
+        	long dy = dx;
+            df = (dx + dy) * (dx + dy);
+            if (x < dx) {
+                df -= (dx - x);
+            }
+            if (y < dy) {
+                df += (dy - y);
+            }
+        } else if (x >= 0 && y < 0) {
+        	long dx = Math.max(Math.abs(x), Math.abs(y));
+        	long dy = -dx;
+            df = (dx + Math.abs(dy)) * (dx + Math.abs(dy) + 1);
+            if (x < dx) {
+                df += (dx - x);
+            }
+            if (y > dy) {
+                df -= (y - dy);
+            }
+        } else if (x < 0 && y <= 0) {
+        	long dx = -Math.max(Math.abs(x), Math.abs(y));
+        	long dy = dx + 1;
+            df = (Math.abs(dx) + Math.abs(dy)) * (Math.abs(dx) + Math.abs(dy));
+            if (x > dx) {
+                df -= (x - dx);
+            }
+            if (y > dy) {
+                df += (y - dy);
+            }
+        } else  if (x <= 0 && y > 0) {
+        	long dx = -Math.max(Math.abs(x), Math.abs(y));
+        	long dy = -dx;
+            df = (Math.abs(dx) + Math.abs(dy)) * (Math.abs(dx) + Math.abs(dy) - 1);
+            if (x > dx) {
+                df += (x - dx);
+            }
+            if (y < dy) {
+                df -= (y - dy);
+            }
+        }
+        System.out.println(df);
+    }
 }
