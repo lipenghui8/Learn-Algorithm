40e36acc87a433b3ed1c52056f4f59a9bb412977
lipenghuihenu <493284461@qq.com>
2022-06-14 11:36:43 +0800

    +1

--- Java_Lanqiao_Final/src/practice/calendar.java
+++ Java_Lanqiao_Final/src/practice/calendar.java
@@ -1,7 +1,26 @@
 package practice;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.DayOfWeek;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+
 public class calendar {
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws ParseException {
+		SimpleDateFormat sdf=new SimpleDateFormat("yyyy/mm/dd");
+		Date date=sdf.parse("2001/01/01");
+		Calendar calendar=Calendar.getInstance();
+		calendar.setTime(date);
+		ArrayList<Integer> arrayList=new ArrayList<>();
+		for(int i=1;i<=10;i++) {
+			arrayList.add(i*i);
+		}
+		int cnt=0;
+		for(int i=1;i<=80;i++) {
+			calendar.add(Calendar.DATE, 1);
+			System.out.println(calendar.toInstant().toString());
+		}
 	}
 }
--- Java_Lanqiao_Final/src/t2021/完全日期.java
+++ Java_Lanqiao_Final/src/t2021/完全日期.java
@@ -2,30 +2,41 @@ package t2021;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.DayOfWeek;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 
 public class 完全日期 {
 	public static void main(String[] args) throws ParseException {
-		SimpleDateFormat simpleDateFormat=new SimpleDateFormat("yyyy/mm/dd");
-		Date date=simpleDateFormat.parse("2001/01/01");
+		SimpleDateFormat sdf=new SimpleDateFormat("yyyy/mm/dd");
+		Date date=sdf.parse("2001/01/01");
 		Calendar calendar=Calendar.getInstance();
 		calendar.setTime(date);
-		calendar.add(Calendar.DAY_OF_YEAR, 1);
-		System.out.println(calendar.toInstant());
-		
-		int[] list=new int[10];
+		calendar.add(Calendar.DATE, 1);
 		ArrayList<Integer> arrayList=new ArrayList<>();
-		
 		for(int i=1;i<=10;i++) {
 			arrayList.add(i*i);
 		}
-		for(int i=1;i<=10000;i++) {
-			StringBuilder y=new StringBuilder();
-			StringBuilder m=new StringBuilder();
-			StringBuilder d=new StringBuilder();
-			
+		int cnt=0;
+		for(int i=1;i<=8000;i++) {
+			String d=calendar.toInstant().toString();
+			if(d.substring(0,10).equals("2022-01-01")) {
+				break;
+			}
+			int sum=0;
+			for(int j=0;j<10;j++) {
+				if(j==4||j==7) {
+					continue;
+				}
+				sum+=d.charAt(j)-'0';
+			}
+			System.out.println(sum+"----"+d);
+			if(arrayList.contains(sum)) {
+				cnt++;
+			}
+			calendar.add(Calendar.DATE, 1);
 		}
+		System.out.println(cnt);
 	}
 }
