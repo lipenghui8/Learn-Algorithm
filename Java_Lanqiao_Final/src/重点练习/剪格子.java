bad4c960ef22c3d62480dafb31143ca2f56322f3
lipenghuihenu <493284461@qq.com>
2022-06-17 12:01:07 +0800

    +1

--- Java_Lanqiao_Final/src/重点练习/剪格子.java
+++ Java_Lanqiao_Final/src/重点练习/剪格子.java
@@ -3,23 +3,45 @@ package 重点练习;
 import java.util.Scanner;
 
 public class 剪格子 {
-	int[][] s=new int[11][11];
-	int[][] v=new int[11][11];
-	int[] dx= {};
-	int[] dy= {};
+	static int[][] s=new int[11][11];
+	static int[][] v=new int[11][11];
+	static int[] dx= {-1,1,0,0};
+	static int[] dy= {0,0,-1,1};
+	static int sums=0,m,n,res=Integer.MAX_VALUE;
 	
-	void dfs(int x,int y,int sum) {
-		if(sum>=) {
-			
+	static void dfs(int x,int y,int sum,int cnt) {
+		if(sum>=sums) {
+			if(sum==sums&&cnt<res) {
+				res=cnt;
+			}
 			return;
 		}
-		
+		for(int i=0;i<4;i++) {
+			int xx=x+dx[i],yy=y+dy[i];
+			if(xx>=1&&xx<=n&&yy>=1&&yy<=m&&v[xx][yy]==0) {
+				v[xx][yy]=1;
+				dfs(xx, yy, sum+s[xx][yy], cnt+1);
+				v[xx][yy]=0;
+			}
+		}
 	}
 	
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
-		int m=sc.nextInt();
-		int n=sc.nextInt();
-		
+		m=sc.nextInt();
+		n=sc.nextInt();
+		for(int i=1;i<=n;i++) {
+			for(int j=1;j<=m;j++) {
+				s[i][j]=sc.nextInt();
+				sums+=s[i][j];
+			}
+		}
+		if(sums%2!=0) {
+			System.out.println(0);
+			return;
+		}
+		sums/=2;
+		dfs(1,1,s[1][1],1);
+		System.out.println(res);
 	}
 }
