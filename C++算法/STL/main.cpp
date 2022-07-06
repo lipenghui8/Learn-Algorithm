01d50deff55184007086207eb6044fb246aa87ef
lipenghuihenu <493284461@qq.com>
2022-07-06 21:05:02 +0800

    +1

--- C++算法/STL/main.cpp
+++ C++算法/STL/main.cpp
@@ -7,6 +7,11 @@ struct node{
 }n;
 
 vector<node> s;
+bool cmp(int a,int b){
+    if(a<=b){
+        return false;
+    }
+}
 int main()
 {
 //    node m={1,2};
@@ -24,12 +29,43 @@ int main()
 //        cout<<(*it).x;
 //    }
 
-    queue<node> q;
-    node v={1,2};
-    q.push(v);
-    node p=q.front();
-    q.pop();
-    cout<<p.x<<p.y;
+//    queue<node> q;
+//    node v={1,2};
+//    q.push(v);
+//    node p=q.front();
+//    q.pop();
+//    cout<<p.x<<p.y;
+//
+//    while(scanf("%d",&a)==1){
+//        int s=a;
+//    }
+//
+//    while(cin>>a){
+//
+//    }
+    int a[]={1,2,3,4,2,6,4};
+    sort(a,a+7,cmp);
+    for(int i=0;i<7;i++){
+        cout<<a[i];
+    }
+    int *b=new int[10];
+
+    set<int> q;
+    q.insert(7);
+    cout<<*(q.find(9))<<endl;
+
+    int a1=1,a2=2;
+    swap(a1,a2);
+    cout<<a1<<a2<<endl;
+
+    double ss=2.587;
+    printf("%.2f\n",ss);
+    cout<<fixed<<setprecision(2)<<ss<<endl;
+
+    int t[5]={4,2,5,7,1};
+    sort(t,t+5);
+    reverse(t,t+5);
+
 
     return 0;
 }
