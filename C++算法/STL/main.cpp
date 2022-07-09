f9a4597a211d0acd6a039d40b7b5908733e743c1
lipenghuihenu <493284461@qq.com>
2022-07-09 22:40:44 +0800

    +1

--- C++算法/STL/main.cpp
+++ C++算法/STL/main.cpp
@@ -43,28 +43,31 @@ int main()
 //    while(cin>>a){
 //
 //    }
-    int a[]={1,2,3,4,2,6,4};
-    sort(a,a+7,cmp);
-    for(int i=0;i<7;i++){
-        cout<<a[i];
-    }
-    int *b=new int[10];
-
-    set<int> q;
-    q.insert(7);
-    cout<<*(q.find(9))<<endl;
-
-    int a1=1,a2=2;
-    swap(a1,a2);
-    cout<<a1<<a2<<endl;
-
-    double ss=2.587;
-    printf("%.2f\n",ss);
-    cout<<fixed<<setprecision(2)<<ss<<endl;
-
-    int t[5]={4,2,5,7,1};
-    sort(t,t+5);
-    reverse(t,t+5);
+//    int a[]={1,2,3,4,2,6,4};
+//    sort(a,a+7,cmp);
+//    for(int i=0;i<7;i++){
+//        cout<<a[i];
+//    }
+//    int *b=new int[10];
+//
+//    set<int> q;
+//    q.insert(7);
+//    cout<<*(q.find(9))<<endl;
+//
+//    int a1=1,a2=2;
+//    swap(a1,a2);
+//    cout<<a1<<a2<<endl;
+//
+//    double ss=2.587;
+//    printf("%.2f\n",ss);
+//    cout<<fixed<<setprecision(2)<<ss<<endl;
+//
+//    int t[5]={4,2,5,7,1};
+//    sort(t,t+5);
+//    reverse(t,t+5);
+    string a="sss";
+    char b='a';
+    cout<<a+b;
 
     return 0;
 }
---
+++ 机试/图论/Eight/Eight.cpp
@@ -0,0 +1,61 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+int s[4][4];
+int v[4][4];
+int dx[] = { -1,1,0,0 };
+int dy[] = { 0,0,-1,1 };
+string dd[] = { "u","d","l" ,"r" };
+struct node
+{
+    int x, y;
+    string path = "";
+};
+void bfs(int nx,int ny)
+{
+    queue<node> q;
+    q.push({nx,ny,""});
+    v[nx][ny] = 1;
+    while (!q.empty()) 
+    {
+        node nn = q.front();
+        q.pop();
+        if (nn.x == 3 && nn.y == 3) {
+            for (int i = 1; i <= 3; i++) {
+                for (int j = 1; j <= 3; j++) {
+                    if (j * (i - 1) + j == s[i][j]) {
+
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < 4; i++) {
+            int x = nn.x + dx[i];
+            int y = nn.y + dy[i];
+            string path = nn.path.append(dd[i]);
+            if (x >= 1 && x <= 3 && y >= 1 && y <= 3&&v[x][y]==0) {
+                
+            }
+        }
+    }
+}
+int main()
+{
+    char k;
+    int sx, sy;
+    for (int i = 1; i <= 3; i++) {
+        for (int j = 1; j <= 3; j++) {
+            cin >> k;
+            if (k >= '0' && k <= '9') {
+                s[i][j] = k-'0';
+            }
+            else {
+                s[i][j] = 0;
+                sx = i;
+                sy = j;
+            }
+        }
+    }
+    bfs(sx, sy);
+
+    return 0;
+}
