4c8efa8cb889e9eff622b30ae5ddf87aa3122241
lipenghuihenu <493284461@qq.com>
2022-07-10 19:20:07 +0800

    +1

---
+++ 机试/图论/N皇后问题/N皇后问题.cpp
@@ -0,0 +1,40 @@
+﻿#include<iostream>
+using namespace std;
+int n,sum=0;
+int v[11],res[11];
+int vz[50], vy[50];
+
+void dfs(int x)
+{
+    if (x > n) {
+        sum++;
+        return;
+    }
+    for (int i = 1; i <= n; i++) {
+        if (v[i] == 0 && vz[x + i]==0 && vy[x - i+n]==0) {
+            v[i] = 1;
+            vz[x + i] = 1;
+            vy[x - i + n] = 1;
+            dfs(x + 1);
+            v[i] = 0;
+            vz[x + i] = 0;
+            vy[x - i + n] = 0;
+        }
+    }
+}
+int main()
+{
+    for (int i = 1; i <= 10; i++) {
+        memset(v, 0, sizeof(v));
+        memset(vz, 0, sizeof(vz));
+        memset(vy, 0, sizeof(vy));
+        sum = 0;
+        n = i;
+        dfs(1);
+        res[i] = sum;
+    }
+    while (cin >> n&&n!=0) {
+        cout << res[n] << endl;
+    }
+    return 0;
+}
