5238d1c499302b3d3131565ca001ca32aecc6df1
lipenghuihenu <493284461@qq.com>
2022-07-12 18:12:59 +0800

    +1

---
+++ 机试/图论/Find The Multiple/Find The Multiple.cpp
@@ -0,0 +1,28 @@
+﻿#include<iostream>
+#include<queue>
+using namespace std;
+int n;
+typedef long long LL;
+
+LL bfs(int x) 
+{
+    queue<int> q;
+    q.push(1);
+    while (!q.empty()) {
+        LL xx = q.front();
+        q.pop();
+        if (xx % n == 0) {
+            return xx;
+        }
+        q.push(xx * 10);
+        q.push(xx * 10 + 1);
+    }
+}
+int main()
+{
+    
+    while (cin >> n && n != 0) {
+        cout<<bfs(n)<<endl;
+    }
+    return 0;
+}
