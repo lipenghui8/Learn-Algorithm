44bf1cefcae7c87a8c2fe1c8815f29ec30653d5f
lipenghuihenu <493284461@qq.com>
2022-07-12 17:46:43 +0800

    +1

---
+++ 机试/STL和基本数据结构/士兵队列训练问题/士兵队列训练问题.cpp
@@ -0,0 +1,49 @@
+﻿#include<iostream>
+#include<vector>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int k;
+    while (n--) {
+        cin >> k;
+        vector<int> s;
+        for (int i = 1; i <= k; i++) {
+            s.push_back(i);
+        }
+        int flag = 1;
+        while (s.size() > 3) {
+            if (flag) {
+                int i = 1;
+                for (vector<int>::iterator it=s.begin(); it != s.end();i++) {
+                    if (i % 2 == 0) {
+                        it=s.erase(it);
+                    }
+                    else {
+                        it++;
+                    }
+                }
+                flag = 0;
+            }
+            else {
+                int i = 1;
+                for (vector<int>::iterator it = s.begin(); it != s.end();i++) {
+                    if (i % 3 == 0) {
+                        it = s.erase(it);
+                    }
+                    else {
+                        it++;
+                    }
+                }
+                flag = 1;
+            }
+        }
+        for (int i = 0; i < s.size()-1; i++) {
+            cout<<s[i]<<" ";
+        }
+        cout <<s[s.size()-1]<< endl;
+    }
+    return 0;
+}
