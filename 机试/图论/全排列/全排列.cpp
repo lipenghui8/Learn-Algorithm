19501b59afc88726363e8c22f7e60e12992f234e
lipenghuihenu <493284461@qq.com>
2022-07-24 19:22:42 +0800

    +1

---
+++ 机试/图论/全排列/全排列.cpp
@@ -0,0 +1,27 @@
+﻿class Solution {
+public:
+    int v[10];
+    vector<int> num;
+    vector<vector<int>> res;
+    void dfs(int x, vector<int> r) {
+        if (x > num.size()) {
+            res.push_back(r);
+            return;
+        }
+        for (int i = 0; i < num.size(); i++) {
+            if (v[i] == 0) {
+                r.push_back(num[i]);
+                v[i] = 1;
+                dfs(x + 1, r);
+                v[i] = 0;
+                r.pop_back();
+            }
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        num = nums;
+        vector<int> r;
+        dfs(1, r);
+        return res;
+    }
+};
--- 机试/图论/组合总和/组合总和.cpp
+++ 机试/图论/组合总和/组合总和.cpp
@@ -1,20 +1,28 @@
-﻿// 组合总和.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
-
-#include <iostream>
-
-int main()
-{
-    std::cout << "Hello World!\n";
-}
-
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
+﻿class Solution {
+public:
+    vector<int> c;
+    vector<vector<int>> sres;
+    int t;
+    void dfs(int x, vector<int> res, int p = 0) {
+        if (x >= t) {
+            if (x == t) {
+                sres.push_back(res);
+            }
+            return;
+        }
+        for (int i = p; i < c.size(); i++) {
+            res.push_back(c[i]);
+            x = x + c[i];
+            dfs(x, res, i);
+            x = x - c[i];
+            res.pop_back();
+        }
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        c = candidates;
+        t = target;
+        vector<int> p;
+        dfs(0, p);
+        return sres;
+    }
+};
