298d9680c5760f33b5f9356a2aa4dda51b2ee08d
lipenghuihenu <493284461@qq.com>
2022-09-18 09:40:32 +0800

    +1

---
+++ 机试/STL和基本数据结构/1008/1008.cpp
@@ -0,0 +1,24 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n,a=0,b=0;
+    long long sum = 0;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> a;
+        if (a > b) {
+            sum += (a - b) * 6 + 5;
+        }
+        else if (a < b) {
+            sum += (b - a) * 4 + 5;
+        }
+        else if (a == b) {
+            sum += 5;
+        }
+        b = a;
+    }
+    cout << sum;
+    return 0;
+}
